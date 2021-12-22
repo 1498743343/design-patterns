c0744deb691ad131d581c1f5424a0bee7c2a720e
chenzihan <chenzihan@thunisoft.com>
2021-12-22 16:08:16 +0800

    feat: 新增代理模式代码; 新增 pom.xml 依赖

---
+++ src/main/java/com/chen/proxy/dynamic/jdk/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.chen.proxy.dynamic.jdk;
+
+import com.chen.proxy.dynamic.User;
+
+/**
+ * 用户服务impl
+ *
+ * @author chenzihan
+ * @date 2021/12/22
+ */
+public class UserServiceImpl implements UserService{
+    @Override
+    public int add(User user) {
+        System.out.println("添加用户成功");
+        return 1;
+    }
+
+    @Override
+    public void delete(User user) {
+        System.out.println("删除用户成功");
+    }
+}
