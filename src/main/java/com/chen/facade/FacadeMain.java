2908417c6d99569bedc80e9ed7a003c92c03095d
chenzihan <chenzihan@thunisoft.com>
2021-12-24 16:10:56 +0800

    feat: 新增外观模式代码

---
+++ src/main/java/com/chen/facade/FacadeMain.java
@@ -0,0 +1,20 @@
+package com.chen.facade;
+
+/**
+ * 外观模式 main: 外观模式的作用是为复杂的系统、框架、接口等提供一个简单统一的接口, 使其更方便使用
+ * 外观模式更注重的是对于复杂系统中的某一个功能的封装, 所以它并不是简化系统的银弹, 务必注意是 "某一个", 不一定是一个接口或者方法, 强调的是一小部分功能
+ *
+ * @author chenzihan
+ * @date 2021/12/24
+ */
+public class FacadeMain {
+
+    /**
+     * 此处不再编写示例代码
+     *
+     * @param args args
+     */
+    public static void main(String[] args) {
+
+    }
+}
