e8c0cb43cd28c843db410059f26542f88ca1f923
chenzihan <chenzihan@thunisoft.com>
2021-12-27 15:52:40 +0800

    feat: 新增责任链模式代码

---
+++ src/main/java/com/chen/chain/Handler.java
@@ -0,0 +1,16 @@
+package com.chen.chain;
+
+/**
+ * 处理程序
+ *
+ * @author chenzihan
+ * @date 2021/12/27
+ */
+public interface Handler {
+    /**
+     * 处理
+     *
+     * @param request 请求
+     */
+    void process(Request request);
+}
