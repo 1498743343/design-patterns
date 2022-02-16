0c955c09b2faf090fc39817571aa74cdf8758784
chenzihan <chenzihan@thunisoft.com>
2022-02-16 10:08:04 +0800

    feat: 新增解释器模式代码

---
+++ src/main/java/com/chen/behaviour/interpreter/InterpreterTest.java
@@ -0,0 +1,12 @@
+package com.chen.behaviour.interpreter;
+
+/**
+ * 解释器模式：描述了如何构成一个简单的语言解释器，主要应用在使用面向对象语言开发编译器中；因为不常见，此处不在举例
+ * 我个人认为解释器模式就是根据自己的需求去写一个特定的逻辑或者算法去完成指定的事
+ * 常见的解释器模式有： String 的 matches 方法等
+ *
+ * @author chenzihan
+ * @date 2022/02/16
+ */
+public class InterpreterTest {
+}
