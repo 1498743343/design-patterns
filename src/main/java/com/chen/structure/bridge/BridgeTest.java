28350bc287ea3cc5ecc8f9ec9b2a686d9230f423
chenzihan <1498743343@qq.com>
2023-02-23 22:29:54 +0800

    feat: 修改错误

--- src/main/java/com/chen/structure/bridge/BridgeTest.java
+++ src/main/java/com/chen/structure/bridge/BridgeTest.java
@@ -2,19 +2,15 @@ package com.chen.structure.bridge;
 
 import com.chen.structure.bridge.pojo.BigCar;
 import com.chen.structure.bridge.pojo.FuelEngine;
-import org.junit.Test;
 
 /**
- * 桥测模式：用于把抽象化与实现化解耦，使得二者可以独立变化
+ * 桥接模式：用于把抽象化与实现化解耦，使得二者可以独立变化
  * 说人话就是避免直接继承带来的子类爆炸
  *
  * @author chenzihan
- * @date 2022/02/14
  */
 public class BridgeTest {
-
-    @Test
-    public void t1() {
+    public static void main(String[] args) {
         FuelEngine fuelEngine = new FuelEngine();
         BigCar bigCar = new BigCar(fuelEngine);
         bigCar.drive();
